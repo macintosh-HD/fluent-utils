ec827befede3898033949edffedddc3357257319
Julian Gentges <JulGentges@gmail.com>
2021-09-21 17:05:43 +0200

    support Timestamp properties

--- Sources/FluentUtils/FluentModel.swift
+++ Sources/FluentUtils/FluentModel.swift
@@ -37,3 +37,19 @@ public extension OptionalParentProperty where Model: FluentModel {
         self.init(key: key.key)
     }
 }
+
+public extension TimestampProperty where Model: FluentModel {
+    convenience init(key: Model.FieldKeys, on trigger: TimestampTrigger, format: TimestampFormatFactory<Format>) {
+        self.init(key: key.key, on: trigger, format: format)
+    }
+    
+    convenience init(key: Model.FieldKeys, on trigger: TimestampTrigger, format: Format) {
+        self.init(key: key.key, on: trigger, format: format)
+    }
+}
+
+public extension TimestampProperty where Model: FluentModel, Format == DefaultTimestampFormat {
+    convenience init(key: Model.FieldKeys, on trigger: TimestampTrigger) {
+        self.init(key: key.key, on: trigger)
+    }
+}
